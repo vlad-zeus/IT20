6986dd6ee7b8c50a6451cf1bf375d13f94ec4f5b
vladzeus <v.gorodetsky@gmail.com>
2021-10-28 23:46:04 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	g2_gorodetsky_vladislav_dz_10/index.html

---
+++ 02_JavaScript/g2_gorodetsky_vladislav_dz_01/js/script02_1_v2.js
@@ -0,0 +1,27 @@
+function days_in_word(x) {
+    if (x === 0 || (x >= 5 && x <= 20)) {
+        return  "дней";
+    } else if (x === 1) {
+        return "день";
+    } else if (x >= 2 && x <= 4) {
+        return "дня";
+    }
+}
+
+let a = prompt("Введите число", "0");
+let days;
+let last_day = a % 100;
+if (Number(a) >= 0) {
+    if (last_day >= 0 && last_day <= 20) {
+        alert(a + ' ' + days_in_word(last_day));
+    } else {
+        last_day = last_day % 10;
+        alert(a + ' ' + days_in_word(last_day));
+    }
+} else {
+    alert("Введите положительное число!")
+    window.location.reload();
+}
+
+
+
