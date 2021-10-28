1407a10e8128e5c5a4ce5956855fe32f1547cb88
vladzeus <v.gorodetsky@gmail.com>
2021-10-28 08:56:53 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	g2_gorodetsky_vladislav_dz_10/index.html

---
+++ .history/02_JavaScript/g2_gorodetsky_vladislav_dz_01/js/script_20211026233641.js
@@ -0,0 +1,43 @@
+const NAME = "Владислав";
+const SURNAME = "Городецкий";
+const DATE_OF_BIRTH =
+  new Date(1979, 11, 26).getDate +
+  "." +
+  new Date(1979, 11, 26).getMonth() +
+  "." +
+  new Date(1979, 11, 26).getFullYear();
+let age = Math.floor(
+  (new Date() - DATE_OF_BIRTH) / (1000 * 3600 * 24 * 365.25)
+);
+let height = "181 см";
+let sex = "мужской";
+let weight = "129 кг";
+let weight = "129 кг";
+const COLOR_OF_EYES = "зеленый";
+let color_of_hair = "седой";
+let family_status = "женат";
+let child = "1";
+let animals = "кошка";
+let profession = "системный администратор, DBA";
+let favorite_music_style = "power metal, trash metal, industrial, darkwave";
+let favorite_sport =
+  "силовой тренинг - для себя, Formula1, снукер - посмотреть";
+let hobby = "почитать книгу";
+
+console.log(
+  " Имя: " + NAME + "\n",
+  "Фамилия: " + SURNAME + "\n",
+  "Дата рождения: " + DATE_OF_BIRTH,
+  "Возраст: " + age + "\n",
+  "Рост: " + height + "\n",
+  "Вест: " + weight + "\n",
+  "Цвет глаз: " + COLOR_OF_EYES + "\n",
+  "Цвет волос: " + color_of_hair + "\n",
+  "Семейное положение: " + family_status + "\n",
+  "Дети: " + child + "\n",
+  "Животные: " + animals + "\n",
+  "Профессия: " + profession + "\n",
+  "Музыкальные предпочтения: " + favorite_music_style + "\n",
+  "Спорт: " + favorite_sport + "\n",
+  "Хобби: " + hobby + "\n"
+);
--- 02_JavaScript/g2_gorodetsky_vladislav_dz_01/js/script.js
+++ 02_JavaScript/g2_gorodetsky_vladislav_dz_01/js/script.js
@@ -1,32 +1,41 @@
-  const NAME = 'Владислав';
-  const SURNAME = 'Городецкий';
-  const DATE_OF_BIRTH = new Date(1979, 11, 26);
-  let   age = Math.floor((new Date() - DATE_OF_BIRTH)/(1000 * 3600 * 24 * 365.25));
-  let   height = '181 см';
-  let   weight = '129 кг';
-  const COLOR_OF_EYES = 'зеленый';
-  let   color_of_hair = 'седой';
-  let   family_status = 'женат'
-  let   child = '1';
-  let   animals = 'кошка';
-  let   profession = 'системный администратор, DBA';
-  let   favorite_music_style = 'power metal, trash metal, industrial, darkwave';
-  let   favorite_sport = 'силовой тренинг - для себя, Formula1, снукер - посмотреть';
-  let   hobby = 'почитать книгу';
+function compute_age(x) {
+  return Math.floor((new Date() - x) / (1000 * 3600 * 24 * 365.25));
+}
 
-  
-  console.log(' Имя: ' + NAME + '\n',
-              'Фамилия: ' + SURNAME + '\n',
-              'Дата рождения: ' + DATE_OF_BIRTH.getDate() + '.' + DATE_OF_BIRTH.getMonth() + '.' + DATE_OF_BIRTH.getFullYear() + '\n',
-              'Возраст: ' + age + '\n',
-              'Рост: ' + height + '\n',
-              'Вест: ' + weight + '\n',
-              'Цвет глаз: ' + COLOR_OF_EYES + '\n',
-              'Цвет волос: ' + color_of_hair + '\n',
-              'Семейное положение: ' + family_status + '\n',
-              'Дети: ' + child + '\n',
-              'Животные: ' + animals + '\n',
-              'Профессия: ' + profession + '\n',
-              'Музыкальные предпочтения: ' + favorite_music_style + '\n',
-              'Спорт: ' + favorite_sport + '\n',
-              'Хобби: ' + hobby + '\n')
+function format_date(d) {
+  return d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+}
+
+const NAME = "Владислав";
+const SURNAME = "Городецкий";
+const DATE_OF_BIRTH = new Date(1979, 11 , 26);
+let age = compute_age(DATE_OF_BIRTH);
+const sex = "мужской";
+let height = "181 см";
+let weight = "129 кг";
+const COLOR_OF_EYES = "зеленый";
+let family_status = "женат";
+let child = 1;
+let animals = "кошка";
+let profession = "системный администратор, DBA";
+let favorite_music_style = "power metal, trash metal, industrial, darkwave";
+let favorite_sport = "силовой тренинг - для себя, Formula1, снукер - посмотреть";
+let hobby = "прочтение книги";
+
+console.log(
+  "Имя: " + NAME,
+  "\n" + "Фамилия: " + SURNAME,
+  "\n" + "Дата рождения: " + format_date(DATE_OF_BIRTH),
+  "\n" + "Возраст: " + age,
+  "\n" + "Пол: " + sex,
+  "\n" + "Рост: " + height,
+  "\n" + "Вест: " + weight,
+  "\n" + "Цвет глаз: " + COLOR_OF_EYES,
+  "\n" + "Семейное положение: " + family_status,
+  "\n" + "Дети: " + child,
+  "\n" + "Животные: " + animals,
+  "\n" + "Профессия: " + profession,
+  "\n" + "Музыкальные предпочтения: " + favorite_music_style,
+  "\n" + "Спорт: " + favorite_sport,
+  "\n" + "Хобби: " + hobby
+);
