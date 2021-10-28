cedf17aae4698b27e402e23f65351a5eb034e581
vladzeus <v.gorodetsky@gmail.com>
2021-10-28 10:45:28 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	g2_gorodetsky_vladislav_dz_10/index.html

--- 02_JavaScript/g2_gorodetsky_vladislav_dz_01/js/script.js
+++ 02_JavaScript/g2_gorodetsky_vladislav_dz_01/js/script.js
@@ -1,14 +1,14 @@
 function compute_age(x) {
-  return Math.floor((new Date() - x) / (1000 * 3600 * 24 * 365.25));
+    return Math.floor((new Date() - x) / (1000 * 3600 * 24 * 365.25));
 }
 
 function format_date(d) {
-  return d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+    return d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
 }
 
 const NAME = "Владислав";
 const SURNAME = "Городецкий";
-const DATE_OF_BIRTH = new Date(1979, 11 , 26);
+const DATE_OF_BIRTH = new Date(1979, 11, 26);
 let age = compute_age(DATE_OF_BIRTH);
 const sex = "мужской";
 let height = "181 см";
@@ -23,19 +23,19 @@ let favorite_sport = "силовой тренинг - для себя, Formula1,
 let hobby = "прочтение книги";
 
 console.log(
-  "Имя: " + NAME,
-  "\n" + "Фамилия: " + SURNAME,
-  "\n" + "Дата рождения: " + format_date(DATE_OF_BIRTH),
-  "\n" + "Возраст: " + age,
-  "\n" + "Пол: " + sex,
-  "\n" + "Рост: " + height,
-  "\n" + "Вест: " + weight,
-  "\n" + "Цвет глаз: " + COLOR_OF_EYES,
-  "\n" + "Семейное положение: " + family_status,
-  "\n" + "Дети: " + child,
-  "\n" + "Животные: " + animals,
-  "\n" + "Профессия: " + profession,
-  "\n" + "Музыкальные предпочтения: " + favorite_music_style,
-  "\n" + "Спорт: " + favorite_sport,
-  "\n" + "Хобби: " + hobby
+    "Имя: " + NAME,
+    "\n" + "Фамилия: " + SURNAME,
+    "\n" + "Дата рождения: " + format_date(DATE_OF_BIRTH),
+    "\n" + "Возраст: " + age,
+    "\n" + "Пол: " + sex,
+    "\n" + "Рост: " + height,
+    "\n" + "Вест: " + weight,
+    "\n" + "Цвет глаз: " + COLOR_OF_EYES,
+    "\n" + "Семейное положение: " + family_status,
+    "\n" + "Дети: " + child,
+    "\n" + "Животные: " + animals,
+    "\n" + "Профессия: " + profession,
+    "\n" + "Музыкальные предпочтения: " + favorite_music_style,
+    "\n" + "Спорт: " + favorite_sport,
+    "\n" + "Хобби: " + hobby
 );
