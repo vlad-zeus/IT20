b94a9fdde736d109e48519e5e2e59602c5d89d60
vladzeus <v.gorodetsky@gmail.com>
2021-10-29 06:01:17 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	g2_gorodetsky_vladislav_dz_10/index.html

--- 02_JavaScript/js/script02_2.js
+++ 02_JavaScript/js/script02_2.js
@@ -5,13 +5,13 @@ let wheel_d = Number(b);
 if (length_km !== 0) {
     if (length_km > 0) {
         if (wheel_d >= 15 && wheel_d <= 159) {
-            let wheel_length_sm = Math.PI * wheel_d * 2.54 / 100;
+            let wheel_length_m = Math.PI * wheel_d * 2.54 / 100;
             let wheel_length_km = Math.PI * wheel_d * 2.54 / 100 / 1000;
             let wheel_turn = length_km / wheel_length_km;
             let wheel_turn_4 = wheel_turn * 4;
-            alert("Полный оборот колеса за: " + wheel_length_sm.toFixed(2) + " метров" +
-                "\n" + "Оборотов одной шины: " + wheel_turn.toFixed(2) +
-                "\n" + "Оборотов четырех шин: " + wheel_turn_4.toFixed(2))
+            alert("Полный оборот колеса за: " + wheel_length_m.toFixed(2) + " метров" +
+                "\n" + "Оборотов одного колеса: " + wheel_turn.toFixed(2) +
+                "\n" + "Оборотов четырех колес: " + wheel_turn_4.toFixed(2))
         } else {
             console.log("Неверное значение диаметра шин! Введите положительное число от 15 до 159!")
         }
