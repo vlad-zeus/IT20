3ba3a1173bf9c6b57440cd1904becd105ee4790a
vladzeus <v.gorodetsky@gmail.com>
2021-10-31 13:44:28 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ 02_JavaScript/js/homework_03_1.js
@@ -0,0 +1,29 @@
+/*Последовательно ввести 2 числа.
+  На отрезке из введенных чисел подсчитать количество чисел кратных 6 и сумму этих чисел.
+  Вывести ответ одним уведомлением.*/
+
+let a = prompt("Введите первое число", "0");
+let b = prompt("Введите второе число", "0");
+const MIN_NUMBER = Math.min(Number(a), Number(b));
+const MAX_NUMBER = Math.max(Number(a), Number(b));
+const MULTIPLICITY = 6;
+let sum = 0;
+let count = 0;
+for (let i = MIN_NUMBER; i <= MAX_NUMBER; i++) {
+    if (i !== 0) {
+        if (i % MULTIPLICITY === 0) {
+            console.log(i)
+            sum += i;
+            count += 1;
+        }
+    }
+}
+alert(`Количество чисел кратных ${MULTIPLICITY}: ${count} 
+    \nСумма чисел равна: ${sum}`);
+
+
+
+
+
+
+
---
+++ 02_JavaScript/js/homework_03_2.js
@@ -0,0 +1,30 @@
+/*Написать программу, которая должна за минимальное количество попыток отгадать задуманное пользователем число от 0 до 100.*/
+
+let min_number = 0;
+let max_number = 100;
+let confirm_result = '';
+let temp_result = 0;
+let difference = 0;
+const MAX_COUNT = 10;
+
+for (let i = 0; i < MAX_COUNT; i++) {
+    console.log(i)
+    temp_result = (max_number + min_number) / 2;
+    confirm_result = confirm(`${temp_result} больше загаданного числа?`);
+    if (confirm_result === true) {
+        max_number = temp_result;
+    } else {
+        min_number = temp_result
+    }
+    difference = max_number - min_number;
+    if (difference < 1) {
+        break;
+    }
+}
+alert(`загаданное число ${Math.floor(max_number)}`);
+
+
+
+
+
+
