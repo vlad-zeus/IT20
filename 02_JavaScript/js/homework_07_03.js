3683a7b6fe596ef5c74eb87871f2a2c3d1d50080
vlad-zeus <44255164+vlad-zeus@users.noreply.github.com>
2021-11-11 18:22:51 +0200

    fix date

--- 02_JavaScript/js/homework_07_03.js
+++ 02_JavaScript/js/homework_07_03.js
@@ -4,13 +4,13 @@
 function Calculator(name) {
     this.name = name;
     this.history = [];
-    this.history_read = function (param) {
+    this.read_history = function (param) {
         for (let i = 0; i < param.length; i++) {
             console.log(param[i]);
         }
         return 'Вывод истории завершен'
     };
-    this.history_destruct = function (param) {
+    this.destruct_history = function (param) {
         param.splice(0);
         return 'История очищена!'
     };
@@ -22,7 +22,7 @@ function Calculator(name) {
             }
         }
         return result_arr;
-    }
+    };
     this.calculate_result = function (param1, param2) {
         let clean_arg = this.check_param(param2);
         let math_symbol;
@@ -53,29 +53,29 @@ function Calculator(name) {
             result = eval(result + math_symbol + clean_arg[j]);
         }
 
-        let date_for_history = new Date().getDate() + '.' + eval(new Date().getMonth() + 1) + '.' + new Date().getFullYear() + ' ' + new Date().getHours() + ':' + new Date().getMinutes();
+        let date_for_history = new Date().toLocaleString();
         this.history.push(`${this.name} ${date_for_history}: ${word_for_history} = ${result}, (${clean_arg})`)
         return result;
-    }
+    };
     this.summation = function (...arg) {
         return this.calculate_result('sum', arg);
     }
     this.subtraction = function (p1, p2) {
         return this.calculate_result('sub', arg);
-    }
+    };
     this.multiplication = function (...arg) {
         return this.calculate_result('mul', arg);
-    }
+    };
     this.segment = function (p1, p2) {
         return this.calculate_result('seg', arg);
-    }
+    };
 }
 
 let calculator = new Calculator('my_calc');
 console.log(calculator.summation(1, 2, 'dfyz', 48));
 console.log(calculator.subtraction(25, 88));
 console.log(calculator.multiplication(18, 28, 'dfyz', 48));
-console.log(calculator.history_read(calculator.history));
-console.log(calculator.history_destruct(calculator.history));
+console.log(calculator.read_history(calculator.history));
+console.log(calculator.destruct_history(calculator.history));
 console.log(calculator.segment(98, 19));
-console.log(calculator.history_read(calculator.history));
+console.log(calculator.read_history(calculator.history));
