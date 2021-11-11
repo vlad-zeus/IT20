a34e6e7a9eb46d5998bb88c6ab7f2f86e59af9a8
vladzeus <v.gorodetsky@gmail.com>
2021-11-11 21:48:14 +0200

    Merge remote-tracking branch 'origin/master'

--- 02_JavaScript/js/homework_07_03.js
+++ 02_JavaScript/js/homework_07_03.js
@@ -10,10 +10,12 @@ function Calculator(name) {
         }
         return 'Вывод истории завершен'
     };
+
     this.destruct_history = function (param) {
         param.splice(0);
         return 'История очищена!'
     };
+
     this.check_param = function (param) {
         let result_arr = [];
         for (let i = 0; i < param.length; i++) {
@@ -23,11 +25,11 @@ function Calculator(name) {
         }
         return result_arr;
     };
+
     this.calculate_result = function (param1, param2) {
         let clean_arg = this.check_param(param2);
         let math_symbol;
         let word_for_history;
-        let clean_arg_length = clean_arg.length;
         switch (param1) {
             case 'sum':
                 math_symbol = '+';
@@ -36,7 +38,6 @@ function Calculator(name) {
             case 'sub':
                 math_symbol = '-';
                 word_for_history = 'разность'
-                clean_arg.splice(2);
                 break;
             case 'mul':
                 math_symbol = '*';
@@ -45,29 +46,29 @@ function Calculator(name) {
             case 'seg':
                 math_symbol = '/';
                 word_for_history = 'частное'
-                clean_arg.splice(2);
                 break;
         }
+
         let result = clean_arg[0];
         for (let j = 1; j < clean_arg.length; j++) {
             result = eval(result + math_symbol + clean_arg[j]);
         }
 
-        let date_for_history = new Date().toLocaleString();
+        let date_for_history = (new Date().toLocaleString()).replace(',', '');
         this.history.push(`${this.name} ${date_for_history}: ${word_for_history} = ${result}, (${clean_arg})`)
         return result;
     };
     this.summation = function (...arg) {
         return this.calculate_result('sum', arg);
-    }
+    };
     this.subtraction = function (p1, p2) {
-        return this.calculate_result('sub', arg);
+        return this.calculate_result('sub', arguments);
     };
     this.multiplication = function (...arg) {
         return this.calculate_result('mul', arg);
     };
     this.segment = function (p1, p2) {
-        return this.calculate_result('seg', arg);
+        return this.calculate_result('seg', arguments);
     };
 }
 
