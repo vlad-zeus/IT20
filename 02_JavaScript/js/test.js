ca1c542808829d2b72657d0558e9fc7d7c22c460
vlad <v.gorodetsky@gmail.com>
2021-11-16 23:08:00 +0200

    Merge remote-tracking branch 'origin/master'

--- 02_JavaScript/js/test.js
+++ 02_JavaScript/js/test.js
@@ -1,55 +1,23 @@
-let square = document.createElement('div');
-
-
-function blocks(squareBlue) {
-    let square = document.createElement('div');
-    square.style.height = '50px';
-    square.style.width = '50px';
-    square.style.margin = '10px';
-    square.style.background = '#0000FF';
-    return square;
-}
-
-
-
-function blocks2(squareGreen) {
-    let square2 = document.body.appendChild(blocks());
-
-    square2.style.height = '100px';
-    square2.style.width = '100px';
-    square2.style.background = '#008000';
-
-    return square2;
-}
-
-let i = 0;
-
-function blocks3(index) {
-    let square3 = document.body.appendChild(blocks2());
-    console.log(square3);
-    console.log(index);
-    console.log(square3[index]);
-    if (index % 3 === 0) {
-        square3[index].style.background = '#FF0000';
-    } else {
-        square3[index].style.background = '#FFFF00';
+$(document).ready(function blocks(squareBlue) {
+    for (let i = 0; i < 100; i++) {
+        $('<div></div>').css({
+            'height': '70px',
+            'width': '70px',
+            'background': '#0091ff',
+            'margin': '10px'
+        }).appendTo('body');
     }
 
-    return square3;
-}
 
-// setTimeout(blocks, 3000);
-// setTimeout(blocks2, 6000);
-// setTimeout(blocks3, 9000);
+    function  blocks2 (squarePurple) {
+        let elements = $('div');
+            elements.css({
+            'height' : '100px',
+            'width' : '100px',
+            'background' : '#800080',
+            'margin': '10px'
+        });
 
-
-
-for (let i = 0; i < 10; i++) {
-    document.body.appendChild(blocks());
-}
-for (let i = 0; i < 10; i++) {
-    document.body.appendChild(blocks2());
-}
-for (let i = 0; i < 10; i++) {
-    document.body.appendChild(blocks3(i));
-}
+    }
+    setTimeout(blocks2, 4000, 1000);
+})
