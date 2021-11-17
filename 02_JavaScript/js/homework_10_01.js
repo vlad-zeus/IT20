c183c38d0317d049913a443d64c96d78a2c14335
vlad <v.gorodetsky@gmail.com>
2021-11-17 14:36:17 +0200

    Merge remote-tracking branch 'origin/master'

--- 02_JavaScript/js/homework_10_01.js
+++ 02_JavaScript/js/homework_10_01.js
@@ -6,23 +6,20 @@
 
 $(document).ready(function () {
 
-    $(create_container = function () {
-        $('<div class="container"></div>')
-            .css({
-                "margin": "0 auto!",
-                "display": "flex",
-                "flex-direction": "column",
-                "align-items": "center",
-            })
-            .prependTo("body");
+        $(create_container = function () {
+            $('<div class="container"></div>')
+                .css({
+                    "margin": "0 auto!",
+                    "display": "flex",
+                    "flex-direction": "column",
+                    "align-items": "center",
+                })
+                .prependTo("body");
+        });
+
+        function get_calendar_event(date1, date2) {
+            let link = JSON.stringify(`https://www.googleapis.com/calendar/v3/calendars/vgorodetsky@gmail.com/events?key=AIzaSyCWGk2otBltS3FbSdEEimN4FypzUGokk3Q&singleEvents=true&orderBy=starttime&timeMin=${date1}&timeMax=${date2}`)
+            console.log(link);
+        }
+    get_calendar_event('2021-10-03T10:00:00-07:00', '2021-12-31T10:00:00-07:00');
     });
-
-    $(get_calendar_event = function (date1, date2) {
-        let link = JSON.stringify(`https://www.googleapis.com/calendar/v3/calendars/vgorodetsky@gmail.com/events?key=AIzaSyCWGk2otBltS3FbSdEEimN4FypzUGokk3Q&singleEvents=true&orderBy=starttime&timeMin=${date1}&timeMax=${date2}`)
-        console.log(link)
-    });
-
-    get_calendar_event('2021-10-03T10:00:00-07:00', '2021-12-31T10:00:00-07:00')
-
-
-});
